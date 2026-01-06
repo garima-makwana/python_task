94f412ac06eead897360671b30ba6f7e1f74e38a
garima-makwana <garimamakwana32@gmail.com>
2026-01-06 09:03:51 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,31 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello world")
+hello world
+>>> x=20
+>>> print(x)
+20
+>>> x=20.5
+>>> print(x)
+20.5
+>>> x=1j
+>>> print(x)
+1j
+>>> x=["apple","banana","cherry"]
+>>> print(x)
+['apple', 'banana', 'cherry']
+>>> x=("banana","apple","cherry")
+>>> print(x)
+('banana', 'apple', 'cherry')
+>>> x=frozenset({"apple","banana","cherry"})
+>>> print(x)
+frozenset({'apple', 'banana', 'cherry'})
+>>> x=True
+>>> print(x)
+True
+>>> x=none
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    x=none
+NameError: name 'none' is not defined. Did you mean: 'None'?
+>>> 

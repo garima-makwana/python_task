77df01b20d29f8807edbde7ef36d433c131c5191
garima-makwana <garimamakwana32@gmail.com>
2026-01-13 11:35:05 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,39 @@
+"""x="Hello"
+print(x)"""
+"""a=14
+b=23
+z=a+b
+print(z)"""
+"""num=int(input("Enter a number:"))
+if num %2==0:
+    print("The number is Even")
+else:
+    print("The number is Odd")"""
+"""year=int(input("Enter a year:"))
+if(year%400==0):
+    print("leap year")
+else:
+    print("Not a leap year")"""
+"""import math
+print(math.pi)"""
+"""PI=3.14159
+print(PI)"""
+"""num=int(input("Enter a number"))
+print("Square=",num**2)"""
+"""import math
+r=float(input("Enter radius:"))
+pi=3.14
+area=pi*r*r
+print("Area of circle=",area)"""
+"""x=10
+print(type(x))"""
+"""import math
+print(math.sqrt(25))
+a=int(input("Enter base:"))
+b=int(input("Enter power:"))
+print("result =",a**b)"""
+num=int(input("Enter a number:"))
+if num>0:
+    print("positive number")
+else:
+    print("negative number")

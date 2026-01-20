e461c955f5986cddd715f41bf7307b4698a2b8b0
garima-makwana <garimamakwana32@gmail.com>
2026-01-20 09:13:48 +0530

    Add files via upload

---
+++ labtask.py
@@ -0,0 +1,36 @@
+"""P=1000
+R=5
+T=2
+SI=(P*R*T)/100
+print("Simple Interest is :",SI)"""
+"""a=14
+b=23
+if a>b:
+    print("Maximum Number is:",a)
+else:
+    print("Maximum Number is:",b)"""
+"""for i in range(1,6):
+    print(i)"""
+"""text="python"
+lenght=len(text)
+print("lenght of string:",lenght)"""
+"""x="Welcome"
+print(x)"""
+"""text="python"
+print("First Character:",text[0])"""
+"""text="python"
+print("last Character:",text[-1])"""
+"""num = 10
+if num > 0:
+    print("Number is Positive")
+else:
+    print("Number is Negative")"""
+"""a = 10
+b = 20
+c = 30
+sum = a + b + c
+print("Sum of three numbers:", sum)"""
+num = int(input("Enter a number: "))
+print("You entered:", num)
+
+

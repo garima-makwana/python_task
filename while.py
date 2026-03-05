5f588a4852d3c0ba2a77cf4e7615073109dd2115
garima-makwana <garimamakwana32@gmail.com>
2026-03-05 19:43:19 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,18 @@
+"""i=1
+while i<=5:
+    print(i)
+    i=i+1"""
+"""i=1
+while i<=10:
+    print(i)
+    i=i+1"""
+"""n=int(input("Enter n:"))
+i=1
+while i<=n:
+    print(i)
+    i=i+1"""
+"""num=int(input("enter number : "))
+i=1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i=i+1"""

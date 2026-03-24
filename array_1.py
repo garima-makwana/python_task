6f5c250ff4ad8dd334c3ae02a15b3e4d01029152
garima-makwana <garimamakwana32@gmail.com>
2026-03-24 07:40:35 +0530

    Add files via upload

---
+++ array_1.py
@@ -0,0 +1,16 @@
+# integer array
+"""from array import array
+arr=array('i',[10,20,30,40])
+print(arr)
+print(type(arr))"""
+# len()-number of element
+"""import array
+arr=array('i',[10,20,30,40,50])
+print(len(arr))"""
+#append()-add element at end
+"""arr=array('i'[10,20,30])
+arr.append(40)
+print(arr)"""
+#insert(pos,x)-insert at position
+from array import array
+arr=array()

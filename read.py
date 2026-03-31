c8005131699adaa624b84a99c547c975ae5f7944
garima-makwana <garimamakwana32@gmail.com>
2026-03-31 07:42:47 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,26 @@
+"""f=open("d1.txt","r")
+data=f.read(9)
+print("first part",data)
+f.close()"""
+
+
+"""f=open("d1.txt","r")
+line1=f.readline()
+line2=f.readline()
+line3=f.readline()
+print("Line 1 : ",line1)
+print("Line 2 : ",line2)
+print("Line 3 : ",line3)
+f.close()"""
+
+
+"""f=open("d1.txt","r")
+lines=f.readlines()
+print("List of lines : ",lines)
+print("Number of lines : ",len(lines))
+f.close()"""
+
+"""f=open("d1.txt","r")
+lines=f.readlines()
+print(lines[1].strip())
+f.close()"""

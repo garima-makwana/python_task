6699305cb5505444f91bf1cb271cc89a79015f06
garima-makwana <garimamakwana32@gmail.com>
2026-04-04 09:03:17 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,13 @@
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4])
+print(arr[:3])
+print(arr[2:])
+print(arr[:])"""
+
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[::2])
+print(arr[1::2])
+print(arr[::3])
